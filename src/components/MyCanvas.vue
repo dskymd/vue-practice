684f4f5ceee4d1f77beae73d06cfdbec4562115d
dskymd <dskymd@gmail.com>
2023-08-17 10:46:16 +0900

    feat: add Canvas View

---
+++ src/components/MyCanvas.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const canvas = ref<HTMLCanvasElement | undefined>()
+
+onMounted(() => {
+
+  const ctx = canvas.value?.getContext("2d");
+  if (!ctx) {
+    return
+  }
+
+  console.log(ctx)
+
+  ctx.fillStyle = "#f90";
+  ctx.fillRect(1, 1, 100, 100);
+
+  ctx.fillStyle = "#990";
+  ctx.fillRect(200, 220, 50, 20);
+
+  console.log(ctx)
+
+
+  ctx.fillStyle = "#f00";
+  ctx.beginPath()
+  // ctx.clearRect(0, 0, 200, 200)
+  ctx.arc(400, 400, 100, 0, Math.PI * 2)
+  ctx.fill()
+
+
+  ctx.font = '48px serif';
+  ctx.fillText('Hello world', 0, 48);
+
+})
+</script>
+
+
+
+<template>
+  <canvas width="900" height="900" class="canvas" id="canvas" ref="canvas"></canvas>
+</template>
+
+
+<style>
+/*  */
+</style>
---
+++ src/views/CanvasView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+// import { onErrorCaptured } from 'vue'
+import MyCanvas from "../components/MyCanvas.vue";
+
+
+// import { useCounterStore } from '@/stores/counter'
+
+// const counter = useCounterStore()
+
+
+// const message = 'Hi!😇'
+
+// onErrorCaptured(() => {
+//   console.log('error')
+// })
+
+</script>
+
+<template>
+  <div class="canvas">
+    <header class="header">
+      <h1>Canvas</h1>
+    </header>
+
+    <div class="body">
+      <MyCanvas id="MyCanvas"></MyCanvas>
+    </div>
+
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .canvas {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+
+    .header {
+      width: 100%;
+      background: #999;
+    }
+
+    .body {
+      width: 100%;
+      /* background: #eee; */
+      /* border: 1px solid #eee; */
+    }
+
+    p {
+      color: #F90;
+    }
+  }
+}
+</style>
